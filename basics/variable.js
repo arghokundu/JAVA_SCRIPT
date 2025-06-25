40e64e3a8ae25a3773662c397bfc611287d39308
arghokundu <155056087+arghokundu@users.noreply.github.com>
2025-06-25 06:11:54 +0000

    container config done

---
+++ basics/variable.js
@@ -0,0 +1,14 @@
+const acId=1234
+let acName="supriyo"
+var acEmail="sup@gmail.com"
+acCity="kgp"
+let acState;
+
+//constant var access but not coz constant var
+//acId=2
+
+console.log(acId);
+
+console.table([acId,acEmail,acName,acState,acCity]);
+
+/* var not use coz var issue scope access and functional access*/
