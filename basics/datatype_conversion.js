088b2022976948f0a3e27ea3eb463cc435ba0425
arghokundu <155056087+arghokundu@users.noreply.github.com>
2025-06-26 07:00:47 +0000

    datatype conversion

---
+++ basics/datatype_conversion.js
@@ -0,0 +1,29 @@
+let s=33
+console.log(typeof(s));
+
+console.log("string to number conversion");
+let s1="33"
+console.log(typeof(s1));
+let ss=Number(s1)
+console.log(ss);
+console.log(typeof(ss));
+
+
+console.log("nan conversion ");
+let s11="33abc"
+console.log(typeof(s11));
+let sss=Number(s11)
+console.log(sss);
+console.log(typeof(sss));
+
+
+//boolean uses
+let b=""
+let b1=Boolean(b)
+console.log(b1);
+
+let bb="1234"
+let b2=Boolean(bb)
+console.log(b2);
+
+
