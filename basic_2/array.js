d1825f734f0aa69096d9c9e0e4608e84d4a2c437
arghokundu <155056087+arghokundu@users.noreply.github.com>
2025-06-28 04:50:28 +0000

    array_1 done

---
+++ basic_2/array.js
@@ -0,0 +1,40 @@
+//array
+const a=[1,2,3,4,5]
+//console.log(a);
+//console.log(a[0]);
+
+//array method
+
+/*a.push(6)
+a.push(7)
+console.log(a);
+a.pop()*/
+
+//a.unshift(9)
+//a.shift()
+
+//console.log(a.includes(4));
+//console.log(a.indexOf(9));
+//console.log(a);
+
+/*const a2=a.join()
+console.log(a);
+console.log(a2);*/
+
+//**************slice   and aplice *************
+
+console.log("A ",a);
+
+const mn1=a.slice(0,3)
+
+console.log("slice array ",mn1);
+console.log("B ",a);
+
+const mn2=a.splice(0,3)
+console.log("splice array ",mn2);
+console.log("C ",a);
+
+
+
+
+
