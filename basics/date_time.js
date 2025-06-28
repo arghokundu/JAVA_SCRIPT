b78810edf0d34d85e324dc76e03378dee26b4783
arghokundu <155056087+arghokundu@users.noreply.github.com>
2025-06-28 04:10:12 +0000

    date done

---
+++ basics/date_time.js
@@ -0,0 +1,24 @@
+//dates
+
+let d=new Date()
+/*console.log(d.toString());
+console.log(d.toDateString());
+console.log(d.toISOString());
+console.log(d.toLocaleString());
+console.log(typeof(d));*/
+
+let dd=new Date(2024,0,23)
+/*console.log(dd.toString());
+
+let ddd=new Date("01-06-2024")
+console.log(ddd.toLocaleString());*/
+
+let t=Date.now()
+//console.log(t);
+//console.log(dd.getTime());
+
+let nd=new Date()
+console.log(nd);
+console.log(nd.getMonth());
+
+
