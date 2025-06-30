5e24ebadb14e0656e044223b030a207e6e8d585b
arghokundu <155056087+arghokundu@users.noreply.github.com>
2025-06-30 19:04:05 +0000

    switch case done

---
+++ control_flow/switch_case.js
@@ -0,0 +1,53 @@
+//**********basic syntax********** */
+/*switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}*/
+
+const month=3
+switch (month) 
+{
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("febuary");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    default:
+        break;
+}
+
+const mon="april"
+switch (mon) 
+{
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("febuary");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    default:
+        break;
+}
