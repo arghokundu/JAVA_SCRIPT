d1a07efa9a4cd460fdcbff873bff6885b2dfdc3f
arghokundu <155056087+arghokundu@users.noreply.github.com>
2025-06-30 18:54:42 +0000

    if normal done

---
+++ control_flow/if_1.js
@@ -0,0 +1,41 @@
+/*const score=200
+
+if(score >100)
+{
+    const power="fly"
+    console.log(`user power ${power}`);
+}
+console.log(`user power ${power}`);*/
+
+const bal=1000
+/*if (bal>5000) console.log("test");*/
+
+/*if (bal<500)
+{
+    console.log("less than");   
+}
+else if(bal<750)
+{
+    console.log("less than 750");
+}
+else
+{
+    console.log("less than 1500"); 
+}*/
+
+const userloggedin=true
+const debitcard=true
+const email=true
+const google=false
+if (userloggedin && debitcard)
+{
+    console.log("buy courses");
+    
+}
+
+if (email || google)
+{
+    console.log("executed");
+}
+
+
