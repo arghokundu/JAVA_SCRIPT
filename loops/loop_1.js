5073e3c816bb9e1ed5404daf684813b974b99777
arghokundu <155056087+arghokundu@users.noreply.github.com>
2025-07-01 05:01:34 +0000

    for basic done

---
+++ loops/loop_1.js
@@ -0,0 +1,22 @@
+//for loop
+//syntax
+/*for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}*/
+
+const a=[1,2,3,4,5,6,7,8]
+let i
+console.log("length of array is:",a.length);
+for(i=0;i<=a.length;i++)
+{
+    const ele=i
+    //console.log(ele);
+    if(ele==5)
+    {
+        console.log("5 is best number");
+    }
+    console.log(ele);
+}
+//console.log(a[i]);
+
