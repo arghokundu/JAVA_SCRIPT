80c72df445a69a3ffe1878f0c46ebc459e61bff6
arghokundu <155056087+arghokundu@users.noreply.github.com>
2025-07-03 10:47:46 +0000

    project 2 done

---
+++ project/project_02/project_02.js
@@ -0,0 +1,43 @@
+const form=document.querySelector('form')
+
+//this use case will gives empty value
+//const height=parseInt(document.querySelector('#height').value)
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+    const height=parseInt(document.querySelector('#height').value)
+    const weight=parseInt(document.querySelector('#weight').value)
+    const results=document.querySelector('#results')
+    if(height=='' || height<0 || isNaN(height))
+    {
+        results.innerHTML=`please give A VALID HEIGHT${height}`
+    }
+    //results.innerHTML=`${height}`
+    else if(weight=='' || weight<0 || isNaN(weight))
+    {
+        results.innerHTML=`please give A VALID HEIGHT${weight}`
+    }
+    else
+    {
+        const bmi=(weight/((height*weight)/10000)).toFixed(2)
+        //show the result
+        results.innerHTML=`<span>${bmi}</span>`
+        
+        //msg printing
+        /*Under Weight = Less than 18.6
+        Normal Range = 18.6 and 24.9
+        Overweight = Greater than 24.9*/
+        switch(bmi)
+        {
+            case 'bmi<18.6':
+                results.innerHTML=`<span>under weight</span>`
+                break
+            case 'bmi>=18.6' & 'bmi<=24.9':
+                results.innerHTML=`<span>normal</span>`
+                break
+            case 'bmi>24.9':
+                results.innerHTML=`<span>overweight</span>`
+        }
+    }
+})
+
